2a8bf3739a02c9239bba40cb001dae88ef0d927e
Paul Arthur <paul.arthur@flowerysong.com>
2024-10-08 16:27:57 +0000

    CI: test mode inference

--- test/test_milter.py
+++ test/test_milter.py
@@ -176,6 +176,34 @@ def test_milter_mode_v(run_miltertest):
     assert msg['ARC-Seal'] == ''
 
 
+def test_milter_mode_none_verify(run_miltertest):
+    res = run_miltertest([])
+
+    assert res.returncode == 0
+    assert res.stderr == ''
+
+    msg = message_from_string(res.stdout)
+
+    assert msg['Authentication-Results'] == 'example.com; arc=none smtp.remote-ip=127.0.0.1'
+    assert msg['ARC-Authentication-Results'] == ''
+    assert msg['ARC-Message-Signature'] == ''
+    assert msg['ARC-Seal'] == ''
+
+
+def test_milter_mode_none_sign(run_miltertest):
+    res = run_miltertest([])
+
+    assert res.returncode == 0
+    assert res.stderr == ''
+
+    msg = message_from_string(res.stdout)
+
+    assert msg['Authentication-Results'] == ''
+    assert msg['ARC-Authentication-Results'] == 'i=1; example.com; arc=none'
+    assert 'ARC-Message-Signature' in msg
+    assert 'cv=none' in msg['ARC-Seal']
+
+
 def test_milter_ar(run_miltertest):
     res = run_miltertest([])
     msg = message_from_string(res.stdout)
